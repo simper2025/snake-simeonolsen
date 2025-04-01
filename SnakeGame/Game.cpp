572b6f8f824d3e4038ff75de81ee4690f8a582ab
Simeon Olsen <simeon.olsen@students.snow.edu>
2025-04-01 15:57:34 -0600

    snake work2

--- SnakeGame/Game.cpp
+++ SnakeGame/Game.cpp
@@ -36,8 +36,8 @@ int main() {
 
     //Uncomment the next four lines to see console color options 0-255
     //for (int i = 0; i < 255; i++) {
-    //    setcolor(i);
-    //    _cprintf("Color option: %i. Hello, world.\n", i);
+        //setcolor(i);
+        //_cprintf("Color option: %i. Hello, world.\n", i);
     //}
 
 	runGame();
